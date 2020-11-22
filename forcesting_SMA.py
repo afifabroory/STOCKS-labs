a514d2284c1a593c1cbfc77ef43362356e9c5d8b
harrisrussels <afif.abroory@gmail.com>
2020-11-22 10:22:22 +0800

    Adding writing HTML files

--- forcesting_SMA.py
+++ forcesting_SMA.py
@@ -5,12 +5,18 @@ priceSum = 0
 mape = []
 mapeSum = 0
 movingAverage = []
-source = input("Yahoo/IDX? ").upper()
+
+source = intput("YAHOO/IDX? ").upper()
 n = int(input("SMA(n)? "))
-stock = input("Kode Saham? ").upper()
+STOCKS = input("KODE SAHAM? ").upper()
+
+# WRITE FILE
+f = file("{}.html".format(STOCKS), 'w')
+
 
 # LOAD JSON FILE
-DATA = json.load(open("STOCK_CHART/{}-{}-{}.json".format(stock,source, "20-11-2020")))
+DATA = json.load(open("STOCK_CHART/{}-{}-{}.json"
+                .format(STOCKS, source, "22-11-2020")))
 
 # GET DATA FROM JSON
 DATE_STOCKS = DATA["chart"]["result"][0]["timestamp"]
@@ -18,52 +24,56 @@ CLOSE_STOCKS = DATA["chart"]["result"][0]["indicators"]["quote"][0]["close"]
 
 # CONVERT TIMESTAMP TO FORMATTED TIME COMPLEXITY: O(N)
 for i in range(0, len(DATE_STOCKS)):
-    DATE_STOCKS[i] = dt.datetime.fromtimestamp(DATE_STOCKS[i]).strftime("%d/%m/%Y")
+    DATE_STOCKS[i] = dt.datetime.fromtimestamp(DATE_STOCKS[i])
+                     .strftime("%d/%m/%Y")
 
 for t in range(0, len(CLOSE_STOCKS)):
+  if (CLOSE_STOCKS[t] == None):
+    CLOSE_STOCKS[t] = 0.0
+
   if (t < n):
     priceSum += float(CLOSE_STOCKS[t])
 
     if (t == (n-1)):
       Mt = float(priceSum/n)
     else:
-      Mt = 0
+      Mt = 0.0
 
   else:
-    priceSum += float(CLOSE_STOCKS[t]) - float(CLOSE_STOCKS[t-n])
+    priceSum += CLOSE_STOCKS[t] - CLOSE_STOCKS[t-n]
     Mt = float(priceSum/n)    
 
   movingAverage.append(float("{:.2f}".format(Mt)))
 
-  mapeSum += abs(CLOSE_STOCKS[t] - movingAverage[t])/CLOSE_STOCKS[t]
-  mape.append(float("{:.2f}".format(100 - (mapeSum/(t+1)) * 100)))
+  if (CLOSE_STOCKS[t] != 0.0):
+    mapeSum += abs(CLOSE_STOCKS[t] - movingAverage[t])/CLOSE_STOCKS[t]
+    mape.append(float("{:.2f}".format(100 - (mapeSum/(t+1)) * 100)))
 
-
-print('SMA(' + str(n) + ')')
-print("DATE\t\tCLOSE\t\tFORECASTING BESOK")
+print("SMA({}) - {}\nDATE\t\tCLOSE\t\t\tFORECASTING BESOK".format(n, STOCKS))
 for i in range(0, len(DATE_STOCKS)):
     actual = "NULL"
     forcasting = "NULL"
 
     if ((i%n) == 0):
-      print('')  
+      f.write('\n')  
 
     if (i >= (n-1)):
       if (movingAverage[i] - (CLOSE_STOCKS[i]) < 0):
-        forcasting = "Forcasting Besok: ⬇️"
+        forcasting = "Forcasting Besok: TURUN"
       elif (movingAverage[i] - (CLOSE_STOCKS[i]) > 0):
-        forcasting = "Forcasting Besok: ⬆️"
+        forcasting = "Forcasting Besok: NAIK"
       else:
-        forcasting = "Forcasting Besok: -"
+        forcasting = "Forcasting Besok: DATAR"
 
     if (CLOSE_STOCKS[i] > CLOSE_STOCKS[i-1]):
-      actual = "[⬆️]"
+      actual = "[NAIK]"
     elif (CLOSE_STOCKS[i] < CLOSE_STOCKS[i-1]):
-      actual = "[⬇️]"
+      actual = "[TURUN]"
     else:
-      actual = "[-]"
+      actual = "[DATAR]"
 
     print(
           str(DATE_STOCKS[i]) + '\t{:.2f}\t{}'.format(CLOSE_STOCKS[i], actual) 
-          + '\t' + str(movingAverage[i]) + "\t{}".format(forcasting) 
+          + '\t\t' + str(movingAverage[i]) + "\t{}\n".format(forcasting) 
          ) 
+print("Tingkat Akurasi Forcasting untuk besok: {}%".format(mape[-1]))
