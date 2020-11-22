22baaf6c79f5fc604d62981f3c02b247bbf7c670
harrisrussels <afif.abroory@gmail.com>
2020-11-22 11:38:10 +0800

    Update v0.1 for [22/NOV/2020]

--- forcesting_SMA.py
+++ forcesting_SMA.py
@@ -1,66 +1,94 @@
 import json
 import datetime as dt
 
+# DECLARING VARIABLE
 priceSum = 0
 mape = []
 mapeSum = 0
 movingAverage = []
 
-source = intput("YAHOO/IDX? ").upper()
+# GET INPUT
+source = input("YAHOO/IDX? ").upper()
 n = int(input("SMA(n)? "))
 STOCKS = input("KODE SAHAM? ").upper()
 
-# WRITE FILE
-f = file("{}.html".format(STOCKS), 'w')
-
+# GET CURRENT DAY/MONTH/YEAR
+currentTime = dt.datetime.now()
 
 # LOAD JSON FILE
 DATA = json.load(open("STOCK_CHART/{}-{}-{}.json"
-                .format(STOCKS, source, "22-11-2020")))
+                  .format(STOCKS, source,
+                  str(currentTime.day) + str(currentTime.month) 
+                  + str(currentTime.year))))
 
 # GET DATA FROM JSON
 DATE_STOCKS = DATA["chart"]["result"][0]["timestamp"]
 CLOSE_STOCKS = DATA["chart"]["result"][0]["indicators"]["quote"][0]["close"]
 
+
 # CONVERT TIMESTAMP TO FORMATTED TIME COMPLEXITY: O(N)
 for i in range(0, len(DATE_STOCKS)):
-    DATE_STOCKS[i] = dt.datetime.fromtimestamp(DATE_STOCKS[i])
-                     .strftime("%d/%m/%Y")
+    DATE_STOCKS[i] = dt.datetime.fromtimestamp(
+      DATE_STOCKS[i]).strftime("%d/%m/%Y")
 
+
+# TRAVARSE TO ALL LIST CLOSE_STOCKS[]. TIME COMPLEXITY: O(N)
 for t in range(0, len(CLOSE_STOCKS)):
+
+  # CHECK DATA IF CLOSE_STOCKS[ITERATION-t] is NONE AND CHANGE TO 0
   if (CLOSE_STOCKS[t] == None):
     CLOSE_STOCKS[t] = 0.0
 
+  # CHECK IF ITERATION-t IN SUBARRAY [0 ... n-1]
   if (t < n):
+
+    # SUM ALL ELEMENT OF CLOSE_STOCKS[] TO priceSum
     priceSum += float(CLOSE_STOCKS[t])
 
+    # CHECK IF ITERATION-t INEDX IS n-1 INDEX AND THEM CALCULATE MA_t+1. 
+    # ELSE MA_t
     if (t == (n-1)):
       Mt = float(priceSum/n)
     else:
       Mt = 0.0
 
   else:
-    priceSum += CLOSE_STOCKS[t] - CLOSE_STOCKS[t-n]
-    Mt = float(priceSum/n)    
 
+    # SUM ELEMENT OF (CLOSE_STOCKS[ITERATION-t] - CLOSE_STOCKS[ITERATION-n]
+    priceSum += CLOSE_STOCKS[t] - CLOSE_STOCKS[t-n]
+    Mt = float(priceSum/n) # CALCULATE MA_t+1
+
+  # ADD MA_t+1 RESULT TO LIST movingAverage[]
   movingAverage.append(float("{:.2f}".format(Mt)))
 
-  if (CLOSE_STOCKS[t] != 0.0):
-    mapeSum += abs(CLOSE_STOCKS[t] - movingAverage[t])/CLOSE_STOCKS[t]
-    mape.append(float("{:.2f}".format(100 - (mapeSum/(t+1)) * 100)))
+  # CALCULATE MAPE AND ADD CALCULATION TO MAPE[] TO PERCENTAGE FORMAT.
+  # FORMULA: MAPE = 1/n SUM(At - Ft/At) * 100
+  # n             : Number of times the summuation iteration happen
+  # At            : Actual Value
+  # Ft            : Forcast Value
+  mapeSum += (abs(CLOSE_STOCKS[t] - movingAverage[t])/CLOSE_STOCKS[t])
+  mape.append(float("{:.2f}".format(100 - ((mapeSum/(t+1) * 100)))))
 
+
+f = open("data/html/{}.html".format(STOCKS), 'w') # LOAD FILE/CREATE FILE
+
+# WRITE TO HTML
 f.write("""
 <pre>
-SMA({}) - {}\nDATE\t\tCLOSE\t\t\tFORECASTING BESOK
-</pre>
-""".format(n, STOCKS))
+SMA({}) - {}\n\nDATE\t\tCLOSE\t\t\tFORECASTING BESOK""".format(n, STOCKS))
 for i in range(0, len(DATE_STOCKS)):
+
+    # DEFAULT VALUE FOR ACTUAL AND FORCASTING
     actual = "NULL"
     forcasting = "NULL"
 
+    # MULTIPLE OF N WRITE NEW LINE
     if ((i%n) == 0):
       f.write('\n')  
 
+    # CHECK UP/DOWN/SIDE OF SUBARRAY 
+    # CLOSE_STOCKS[n-1 ... DATE_STOCKS.length - 1]
+    # AND movingAverage[movingAverage[n-1 ... DATE_STOCKS.length - 1]]
     if (i >= (n-1)):
       if (movingAverage[i] - (CLOSE_STOCKS[i]) < 0):
         forcasting = "Forcasting Besok: [TURUN]"
@@ -69,6 +97,7 @@ for i in range(0, len(DATE_STOCKS)):
       else:
         forcasting = "Forcasting Besok: [DATAR]"
 
+    # CHECK UP/DOWN/SIDE OF CLOSE_STOCKS[i] AND CLOSE_STOCKS[i-1] 
     if (CLOSE_STOCKS[i] > CLOSE_STOCKS[i-1]):
       actual = "[NAIK]"
     elif (CLOSE_STOCKS[i] < CLOSE_STOCKS[i-1]):
@@ -76,8 +105,12 @@ for i in range(0, len(DATE_STOCKS)):
     else:
       actual = "[DATAR]"
 
+    # WRITE TO HTML
     f.write(
           str(DATE_STOCKS[i]) + '\t{:.2f}\t{}'.format(CLOSE_STOCKS[i], actual) 
           + '\t\t' + str(movingAverage[i]) + "\t{}\n".format(forcasting) 
          ) 
-f.write("""Tingkat Akurasi Forcasting untuk besok: {}%""".format(mape[-1]))
+
+# WRITE TO HTML
+f.write("""
+Tingkat Akurasi Forcasting untuk besok: {}%</pre>""".format(mape[-1]))
